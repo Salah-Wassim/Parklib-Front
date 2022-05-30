d0b78896f2fc84ab438858d5c6cedb15f23b6bdf
styve Lioumba <styvelioumba98@gmail.com>
2022-05-30 19:15:01 +0200

    _

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView} from 'react-native';
+import {StatusBarComponent} from './components/statusbar.component';
+import {AppBarComponent} from './components/appbar.component';
+import {Text,Stack,VStack, TextInput, IconButton ,Button} from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import {reinitialisationStyle} from './css/reinitialisation.style';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={reinitialisationStyle.container}>
+
+      <StatusBarComponent/>
+      <AppBarComponent/>
+
+      <VStack>
+        
+        <Text variant="h5" style={reinitialisationStyle.textEntete} >
+            Réinitialiser le mot de passe
+          </Text>
+
+        <Stack spacing={2} style={reinitialisationStyle.content}>
+
+          <TextInput
+            label="Mot de passe"
+            variant="outlined"
+            trailing={props => (
+              <IconButton icon={props => <Icon name="eye" {...props} />} {...props} />
+            )}
+          />
+
+          <TextInput
+            label="Confirmer le mot de passe"
+            variant="outlined"
+            trailing={props => (
+              <IconButton icon={props => <Icon name="eye" {...props} />} {...props} />
+            )}
+          />
+
+
+          <Button title="Rénitialiser" style={reinitialisationStyle.btnSubmit}/>
+
+        </Stack>
+      </VStack>
+
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ css/appbar.style.js
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+import SIZES from '../utils/sizing.constant';
+
+export const appbarStyle = StyleSheet.create({
+    container:{
+        width:SIZES.FULL
+    }
+})
