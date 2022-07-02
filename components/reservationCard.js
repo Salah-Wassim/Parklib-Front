425c091991b5a32f4e47402b963862f90b783daf
Salah-Wassim <salahwassim.arfae1@gmail.com>
2022-07-02 14:30:12 +0200

    Creation de la page reservation et du composant reservationCard

---
+++ components/reservationCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, StyleSheet, Text} from "react-native";
+
+const reservationCard = (props) => {
+    return (
+        <View>
+            <Text></Text>
+
+        </View>
+    )
+}
--- pages/reservation.js
+++ pages/reservation.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, Image, Text} from 'react-native';
+import {Button} from '@react-native-material/core';
+import {Stack} from 'react-native-flex-layout';
 
 const Reservation = ({navigation}) => {
     return (
-        <View>
-
-        </View>
+        <Stack m={20} space={20} style={styles.reservationContainer}>
+            <View style={styles.imageContainer}>
+                <Image/>
+            </View>
+            <View style={styles.recapContainerExt}>
+                <View style={styles.recapContainerInt}>
+                    <Text>Parking Aquitaine</Text>
+                    <Text>5 rue Paster 33000 Bordeaux</Text>
+                </View>
+            </View>
+            <View>
+                <Button title='Continuer' onPress={() => navigation.navigate('')}/>
+            </View>
+        </Stack>
     )
 }
 
 const styles = StyleSheet.create({
-
+    reservationContainer: {
+        height:'95%',
+        justifyContent:'space-between'
+    },
+    recapContainerExt:{
+        backgroundColor: '#157575',
+        borderRadius:5,
+        marginBottom:'15px'
+    },
+    recapContainerInt: {
+        padding:9,
+        height:211
+    }
 })
 
 export default Reservation
