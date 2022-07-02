2d00972a1ee789347f5d7935d5be1a2c6ad965b6
Salah-Wassim <salahwassim.arfae1@gmail.com>
2022-07-02 14:39:41 +0200

    Creation page map

---
+++ pages/map.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, StyleSheet, Text} from 'react-native'
+
+const map = () => {
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default map;
