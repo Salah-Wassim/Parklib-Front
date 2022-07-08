dcb22597422d4c578d2f7bd3db27ba98a2a1f8c5
Salah-Wassim <salahwassim.arfae1@gmail.com>
2022-07-08 15:50:22 +0200

    Developpement of the parking search functionality with the implementation of an api that return parking data in real time

--- pages/map.js
+++ pages/map.js
@@ -1,51 +1,73 @@
 import React from "react";
 import {View, StyleSheet, Text, ActivityIndicator, FlatList} from 'react-native';
 
-import ParkingItem from "../components/parkingItem";
-// import MapboxGL from "@rnmapbox/maps";
+import {getParkingSearchedText} from '../api/api'
 
+// import MapboxGL from "@rnmapbox/maps";
 // MapboxGL.setAccessToken("pk.eyJ1Ijoic3R5dmVsaW91bWJhIiwiYSI6ImNsNTN2bGtlMzB0NTEzYnFxeHZuMnRmajcifQ.-y76qVK6qz-9BmgA5sthYw");
 
 const Map = ({navigation, route}) => {
     const {
-        parkings, 
-        isLoading, 
-        //page, 
-        //totalPage, 
-        //loadParkings
+        text
     } = route.params;
+    const [parkings, setParkings ] = React.useState([])
+    const [loading, setloading] = React.useState(false)
 
-    const displayLoading = () => {
-        if(isLoading){
-            return(
-                <View>
-                    <ActivityIndicator size="large"/>
-                </View>
-            )
+    React.useEffect(() => {
+        if(text.length > 0){
+            setloading(true)
+            getParkingSearchedText(text).then(data => {
+                //console.log(data)
+                const resultParking = data.features.map(feature => {
+                    return {
+                        geometry : {
+                            coordinates : []
+                        },
+                        properties : {
+                            gid : feature.properties.gid,
+                            nom : feature.properties.nom,
+                            libres : feature.properties.libres,
+                            total : feature.properties.total,
+                            etat : feature.properties.etat,
+                            url : feature.properties.url,
+                            adresse : feature.properties.adresse,
+                            infor : feature.properties.infor,
+                            secteur : feature.properties.secteur,
+                            ta_type : feature.properties.ta_type,
+                        }
+                    }
+                })
+                setParkings(parking => parking.push({...resultParking}))
+                console.log(parkings)
+            })
         }
-    }
-
-    const displayDetailParking = (id) => {
-        navigation.navigate('FicheParking', {id : id})
-    }
+    }, [])
+        
+    //const displayLoading = () => {
+    //        return(
+    //            <View>
+    //                <ActivityIndicator size="large"/>
+    //            </View>
+    //        )
+    //} 
+    // st_park_p
+    //const displayDetailParking = (id) => {
+    //    navigation.navigate('FicheParking', {id : id})
+    //}
 
     return(
         <View style={styles.page}>
-            {/* <Text>{JSON.stringify(text)}</Text> */}
-            <FlatList
-                data={parkings}
-                keyExtractor = {(item) => item.id.toString()}
-                renderItem={({item}) => <ParkingItem parking={item} displayDetailParking={displayDetailParking}/> }
-                //onEndReachedThreshold={0.5}
-                //onEndReached={() => {
-                //    if(page < totalPage){
-                //        loadParkings
-                //    }
-                //}}
-            />
-            {displayLoading()}
+            {parkings.length > 0 ? <Text>COUCOU</Text> : <Text>Erreur</Text> }
         </View>
     )            
+}
+
+function List(){
+
+    return(
+        <Text>PARKINGS TROUVE</Text>
+    )
+
 }
 
 const styles = StyleSheet.create({
