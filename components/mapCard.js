db6ce54bc60f0aa64cfd974d0458531168a525d8
Ken B <kenb9027@gmail.com>
2022-09-06 11:28:50 +0200

    add default zoom on Bordeaux when map opening

--- components/mapCard.js
+++ components/mapCard.js
@@ -9,10 +9,24 @@ const MapCard = ({isvisible,parkings,setVisible})=>{
 
     const dispatch = useDispatch()
 
+
+    // zoom on Bordeaux city
+    let {width, height} = Dimensions.get('window');
+    const ASPECT_RATIO = width / height;
+    const LATITUDE_DELTA = 0.5; //Increase or decrease the zoom level dynamically
+    const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
     return (
         <View style={styles.container}>
             <MapView
-                style={styles.map}>
+                style={styles.map}
+                region={{
+                    latitude: 44.837789,
+                    longitude: -0.57918,
+                    latitudeDelta: LATITUDE_DELTA,
+                    longitudeDelta: LONGITUDE_DELTA,
+                   }}
+            >
                 {
                     parkings.map(parking => (
                         <Marker
@@ -28,6 +42,8 @@ const MapCard = ({isvisible,parkings,setVisible})=>{
                         />
                     ))
                 }
+                
+                
             </MapView>
 
         </View>
