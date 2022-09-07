805e37f128068f371340425efbb0ffee4f0f1efe
styve Lioumba <styvelioumba98@gmail.com>
2022-09-07 15:12:48 +0200

    add suggestion of address in search

--- api/api.js
+++ api/api.js
@@ -7,6 +7,13 @@ export const getParkingSearchedText = async (text, page) =>{
     .catch(error => {console.log(error)})
 }
 
+export const getAddress = async (text) =>{
+    const url  = 'https://api-adresse.data.gouv.fr/search/?q='+text+'&type=housenumber&autocomplete=1'
+    return await fetch(url)
+    .then(response => response.json())
+    .catch(error => {console.log(error)})
+}
+
 export const getParkingMap = async () => {
     const url  = 'https://data.bordeaux-metropole.fr/api/cub.xjs?' + 'key=' + API_TOKEN 
     return await fetch(url, {mode: 'no-cors'})
