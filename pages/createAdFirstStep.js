007dcc1ae0354356eeec29ebdd8f4c474a45d4c0
Salah-Wassim <salahwassim.arfae1@gmail.com>
2022-10-24 14:36:29 +0200

    development of the first step and installation of the react-native-picker-select component.

--- pages/createAdFirstStep.js
+++ pages/createAdFirstStep.js
@@ -1,16 +1,67 @@
 import React from "react";
-import {View, StyleSheet} from "react-native";
+import {View , StyleSheet , TouchableOpacity} from "react-native";
+import {Text, TextInput, Button} from '@react-native-material/core';
+import { Stack} from 'react-native-flex-layout';
+import RNPickerSelect from 'react-native-picker-select';
 
 const createAdFirstStep = ({navigation}) => {
     return (
-        <View>
-
-        </View>
+        <Stack m={20} spacing={40} style={styles.createAdFirstStepContainer}>
+            <View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.formText}>Adresse complète de votre bien</Text>
+                    <TextInput
+                        style={styles.formInput}
+                        placeholder='Adresse postale'
+                        variant="outlined"
+                    />
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.formText}>Loyer mensuel</Text>
+                    <TextInput
+                        style={styles.formInput}
+                        placeholder='ex : 1000'
+                        variant="outlined"
+                    />
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.formText}>Type de place</Text>
+                    <RNPickerSelect
+                        onValueChange={(value) => console.log(value)}
+                        items={[
+                            { label: 'Parking privé (sous-sol)', value: 'sous-sol' },
+                            { label: 'Parking privé (aerien)', value: 'aerien' },
+                        ]}
+                    />
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.formText}>Nombre de place</Text>
+                    <TextInput
+                        style={styles.formInput}
+                        variant="outlined"
+                    />
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.formText}>Votre place de parking est-elle assurée ?</Text>
+                    <RNPickerSelect
+                        onValueChange={(value) => console.log(value)}
+                        items={[
+                            { label: 'Oui', value: 'oui' },
+                            { label: 'Non', value: 'non' },
+                        ]}
+                    />
+                </View>
+            </View>
+            <View style={styles.submitButtonContainer} onpress={() => navigation.navigate('CreateAdSecondtSteps')}>
+                <Button style={styles.submitButton} title="Continuer" color="#157575"/>
+            </View>
+        </Stack>
     )
 }
-
 const styles = StyleSheet.create({
+    createAdFirstStepContainer: {
 
+    }
 })
 
 export default createAdFirstStep;
