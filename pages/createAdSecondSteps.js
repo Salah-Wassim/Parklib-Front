d6191df9cf7cdb75091911ef7bebd38808422dd3
Ken B <kenb9027@gmail.com>
2023-05-06 12:43:39 +0200

    add date inputs on second page

--- pages/createAdSecondSteps.js
+++ pages/createAdSecondSteps.js
@@ -1,11 +1,17 @@
 import React from "react";
-import {View, StyleSheet} from "react-native";
-import {Text, TextInput, Button} from '@react-native-material/core';
+import {View, StyleSheet, TouchableOpacity, ScrollView} from "react-native";
+import {Text, TextInput, Button, Flex} from '@react-native-material/core';
 import { Stack} from 'react-native-flex-layout';
 import DropDownPicker from 'react-native-dropdown-picker';
 import COLOR from "../utils/color.constant";
+import { DateTimePicker, DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import moment from "moment";
+
 
 const CreateAdSecondSteps = ({ route, navigation }) => {
+
+    const todayDate = new Date();
+    todayDate.setHours(0, 0, 0, 0);
     
     const { parking, price, typeOfPlace, isAssured } = route.params;
     const [title, setTitle] = React.useState('');
@@ -19,27 +25,72 @@ const CreateAdSecondSteps = ({ route, navigation }) => {
         { label: 'Téléphone', value: 'telephone' },
     ]);
 
+    const [startDate, onSetStartDate] = React.useState(todayDate);
+    const [endDate, onSetEndDate] = React.useState(todayDate);
+
+    const showDepartureDatepicker = () => {
+        const currentMode ="date"
+        const onChange = (event, selectedDate) => {
+            selectedDate.setHours(0, 0, 0, 0);
+            onSetStartDate(selectedDate);
+        };
+        DateTimePickerAndroid.open({
+            value: startDate,
+            onChange,
+            mode: currentMode,
+            is24Hour: true,
+        });
+    };
+
+    const showEndDatepicker = () => {
+        const currentMode ="date"
+        const onChange = (event, selectedDate) => {
+            selectedDate.setHours(0, 0, 0, 0);
+            onSetEndDate(selectedDate);
+        };
+        DateTimePickerAndroid.open({
+            value: startDate,
+            onChange,
+            mode: currentMode,
+            is24Hour: true,
+        });
+    };
+
     const handleSubmit = () => {
+        // console.log('today :' + todayDate);
+        // console.log('start :' + startDate);
+        // console.log('end :' + endDate);
         if (title === '' || description === '' || contact === '') {
             setError('Merci de remplir tous les champs s\'il vous plaît')
+            return;
         }
-        else{
-            setError('');
-            const post = {
-                'title': title,
-                'description': description,
-                'price': price ,
-                'typeOfPlace': typeOfPlace ,
-                'contact': contact,
-                'isAssured': isAssured,
-                'ParkingParticulierId': null, 
-                'ValidationStatusId': 1 
-            }
-            navigation.navigate('CreateAdThirdSteps', {
-                post: post,
-                parking: parking,
-            })
+        if (startDate.getTime() < todayDate.getTime()) {
+            setError('La date de début de disponibilité ne peut pas débuter avant aujourd\'hui.')
+            return;
         }
+        if (endDate.getTime() <= startDate.getTime()) {
+            setError('La date de fin de disponibilité doit être postérieur à celle de début.')
+            return;
+        }
+        setError('');
+        // console.log('ok to send')
+        // return;
+        const post = {
+            'title': title,
+            'description': description,
+            'price': price ,
+            'typeOfPlace': typeOfPlace ,
+            'contact': contact,
+            'isAssured': isAssured,
+            'ParkingParticulierId': null, 
+            'ValidationStatusId': 1,
+            'startDate': startDate,
+            'endDate': endDate,
+        }
+        navigation.navigate('CreateAdThirdSteps', {
+            post: post,
+            parking: parking,
+        })
     }
 
     return (
@@ -62,7 +113,36 @@ const CreateAdSecondSteps = ({ route, navigation }) => {
                         variant="outlined"
                         onChangeText={newDescription => setDescription(newDescription)}
                         value={description}
+                        multiline = {true}
+                        numberOfLines = {4}
                     />
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.formText}>Disponible</Text>
+                    <Flex direction="row" >
+                        <Text style={styles.dateFormText}>Du</Text>
+                        <TouchableOpacity
+                            style={styles.dateFormInput} onPress={showDepartureDatepicker}>
+                            <TextInput
+                                placeholder='Début'
+                                variant="outlined"
+                                editable={false}
+                                onChangeText={onSetStartDate}
+                                value={moment(startDate.toLocaleDateString()).format("DD/MM/YYYY")}
+                            />
+                        </TouchableOpacity>
+                        <Text style={styles.dateFormText}>au</Text>
+                        <TouchableOpacity
+                            style={styles.dateFormInput} onPress={showEndDatepicker}>
+                            <TextInput
+                                placeholder='Fin'
+                                variant="outlined"
+                                editable={false}
+                                onChangeText={onSetEndDate}
+                                value={ moment(endDate.toLocaleDateString()).format("DD/MM/YYYY")}
+                            />
+                        </TouchableOpacity>
+                    </Flex>
                 </View>
                 <View style={[styles.formContainer, {zIndex: 2}]}>
                     <Text style={styles.formText}>Contact</Text>
@@ -105,7 +185,8 @@ const styles = StyleSheet.create({
         marginBottom: 5,
     },
     formText: {
-        marginBottom: 5
+        marginBottom: 5,
+        marginTop: 10
     },
     submitButtonContainer: {
         marginBottom: 10,
@@ -115,7 +196,15 @@ const styles = StyleSheet.create({
         color: COLOR.blanc
     },
     dropdownPicker: {
-    }
+    },
+    dateFormInput:{
+        width:'40%'
+    },
+    dateFormText: {
+        width: '10%',
+        textAlign: 'center',
+        textAlignVertical: 'center'
+    },
 })
 
 export default CreateAdSecondSteps;
