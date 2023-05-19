07be0451dbf28bf42d7fcd59796e677dc080bca2
Paul1465 <paul.fournier.3314@gmail.com>
2023-05-19 10:33:20 +0200

    assign state authenticated and add AuthRouteGuard

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react';
 import {NavigationContainer}  from '@react-navigation/native';
 import 'react-native-gesture-handler';
+import AuthContext from './store/authentification/authContext'; 
+import RouteGuard from './components/routeGuard';
+import AuthRouteGuard from './components/authRouteGuard';
+import { fetchToken } from './store/authentification/auth';
 import SignIn from "./pages/signIn";
 import SignUp from "./pages/signUp";
 import Parameters from "./pages/parameters";
@@ -9,6 +13,7 @@ import ContactForm from "./pages/contactForm";
 import ChangePassword from "./pages/ChangePassword";
 import LandingPage from "./pages/landingPage";
 import PaymentMethod from "./pages/paymentMethod";
+import {createStackNavigator} from "@react-navigation/stack";
 import EditCreditCard from "./pages/editCreditCard";
 import History from "./pages/history";
 import PrivacyPolicy from "./pages/privacyPolicy";
@@ -19,8 +24,6 @@ import CheckoutScreen from "./pages/checkoutScreen";
 import Map from "./pages/map";
 import ParkingDetails from './pages/parkingDetails';
 import MenuBurger from "./components/menuBurger";
-import RouteGuard from './components/routeGuard';
-import {createStackNavigator} from "@react-navigation/stack";
 import CreateAdFirstStep from "./pages/createAdFirstStep";
 import CreateAdSecondSteps from "./pages/createAdSecondSteps";
 import CreateAdThirdSteps from "./pages/createAdThirdSteps";
@@ -31,13 +34,19 @@ import VerificationCodeScreen from "./pages/verficationCodeScreen";
 import ParkingParticulierDetails from './pages/parkingParticulierDetails';
 import DisplayPark from "./pages/displayPark";
 import Reservation from "./pages/reservation";
-import AuthContext from './store/authentification/authContext'; 
 import PostDashboard from './pages/postDashboard';
 
 const Stack = createStackNavigator()
 
 export default function App() {
     const [isAuthenticated, setAuthenticated] = useState(false);
+
+    // Fetch user authentication token at the start of the application.
+    // If the token exists, set `isAuthenticated` state to true.
+    useEffect(() => {
+        fetchToken(setAuthenticated);
+    }, []);
+
     return (
         <Provider store={store}>
             <AuthContext.Provider value={{ isAuthenticated, setAuthenticated }}>
@@ -45,8 +54,15 @@ export default function App() {
                     <Stack.Navigator initialRouteName='DrawerNav'>
                         <Stack.Screen name="PostDashboard" options={{title: 'Vos annonces'}} component={PostDashboard}/>
                         <Stack.Screen name="DrawerNav" options={{ title: 'Park\'Lib', headerShown: false }} component={MenuBurger} />
-                        <Stack.Screen name="SignIn" options={{ title: 'Park\'Lib', headerShown: false }} component={SignIn}/>
-                        <Stack.Screen name="SignUp" options={{ title: 'Park\'Lib', headerShown: false }} component={SignUp}/>
+                        <Stack.Screen name="Profile" options={{ title: 'Mon profil' }}>
+                            {props => <RouteGuard {...props}><Profile /></RouteGuard>}
+                        </Stack.Screen>
+                        <Stack.Screen name="SignIn" options={{ title: 'Inscription' }}>
+                            {props => <AuthRouteGuard {...props}><SignIn /></AuthRouteGuard>}
+                        </Stack.Screen>
+                        <Stack.Screen name="SignUp" options={{ title: 'Connexion' }}>
+                            {props => <AuthRouteGuard {...props}><SignUp /></AuthRouteGuard>}
+                        </Stack.Screen>
                         <Stack.Screen name="Parameters" options={{ title: 'Paramètres' }} component={Parameters}/>
                         <Stack.Screen name="Contact" options={{ title: 'Contact' }} component={Contact}/>
                         <Stack.Screen name="ContactForm" options={{ title: 'Nous contacter' }} component={ContactForm}/>
@@ -56,13 +72,6 @@ export default function App() {
                         <Stack.Screen name="EditCreditCard" options={{ title: 'Modifier une carte bancaire' }} component={EditCreditCard}/>
                         <Stack.Screen name="History" options={{ title: 'Historique' }} component={History}/>
                         <Stack.Screen name="PrivacyPolicy" options={{ title: 'Politique de confidentialité' }} component={PrivacyPolicy}/>
-                        <Stack.Screen name="Profile" options={{ title: 'Mon profil' }}>
-                        {() => (
-                            <RouteGuard>
-                            <Profile />
-                            </RouteGuard>
-                        )}
-                        </Stack.Screen>
                         <Stack.Screen name="CGU" options={{ title: 'CGU' }} component={Cgu}/>
                         <Stack.Screen name="PaymentScreen" options={{title: 'Paiement'}} component={PaymentScreen}/>
                         <Stack.Screen name="CheckoutScreen" options={{title: 'Verification'}} component={CheckoutScreen}/>
